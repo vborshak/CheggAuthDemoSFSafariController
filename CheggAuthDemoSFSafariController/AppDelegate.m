b6c97e1e8494a8c523cf1a653c8e15f9c2c56217
Victor Borshak <vborshak@chegg.com>
2017-11-30 17:38:58 +0200

    Added comments + Refactored

--- CheggAuthDemoSFSafariController/AppDelegate.m
+++ CheggAuthDemoSFSafariController/AppDelegate.m
@@ -17,9 +17,21 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
     return YES;
 }
 
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey, id> *)options
+{
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Incoming URL"
+                                                    message:[url absoluteString]
+                                                   delegate:self
+                                          cancelButtonTitle:@"OK"
+                                          otherButtonTitles:nil];
+    [alert show];
+    
+    return YES;
+}
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- CheggAuthDemoSFSafariController/ViewController.m
+++ CheggAuthDemoSFSafariController/ViewController.m
@@ -7,18 +7,29 @@
 //
 
 #import "ViewController.h"
+@import SafariServices;
+
+// redirect_uri should be ideally set to 'cheggereader://'
+// but since currently we don't have profiles set up with such value on back end,
+// we use https://www.chegg.com/auth/external/vst/mobilecallback as a workaround, which internally redirects to 'cheggereader://'.
+// It demonstrates that it's possible to intercept cheggereader:// redirect using App delegate's application:openURL: method
+
+#define AUTH_URL @"https://www.chegg.com/oidc/authorize?client_id=VST&response_type=token&redirect_uri=https://www.chegg.com/auth/external/vst/mobilecallback&action=login"
 
 @interface ViewController ()
-
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)viewDidAppear:(BOOL)animated  {
+    NSURL *url = [NSURL URLWithString:AUTH_URL];
+    SFSafariViewController *sfvc = [[SFSafariViewController alloc] initWithURL:url];
+    [self presentViewController:sfvc animated:YES completion:nil];    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
