8b5d220366f0fd2dd7361c619b1f524b168e5b04
Victor Borshak <vborshak@chegg.com>
2017-11-30 16:15:17 +0200

    Initial Commit

---
+++ CheggAuthDemoSFSafariController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CheggAuthDemoSFSafariController
+//
+//  Created by Victor Borshak on 11/30/17.
+//  Copyright © 2017 Victor Borshak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
